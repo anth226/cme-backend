23688a01df635eaecb3427a6d5f582b24e7a1f09
Shoaib Mehedi <shoaib.mehedi@gmail.com>
2022-08-25 23:34:19 +0600

    default time

--- migrations/1661444341409-Guild.ts
+++ migrations/1661444341409-Guild.ts
@@ -5,8 +5,8 @@ export class Guild1661444341409 implements MigrationInterface {
     await queryRunner.query(`CREATE TABLE "guilds" (
                 "id" serial NOT NULL,
                 PRIMARY KEY ("id"),
-                "created_at" timestamptz NULL,
-                "updated_at" timestamptz NULL,
+                "created_at" timestamptz NULL DEFAULT now(),
+                "updated_at" timestamptz NULL DEFAULT now(),
                 "guildMembers" integer NULL,
                 "name" character(250) NOT NULL DEFAULT 'guild'
             );`);
--- migrations/1661444366425-GuildUsers.ts
+++ migrations/1661444366425-GuildUsers.ts
@@ -5,8 +5,8 @@ export class GuildUsers1661444366425 implements MigrationInterface {
     await queryRunner.query(`CREATE TABLE "guilds_users" (
                 "id" serial NOT NULL,
                 PRIMARY KEY ("id"),
-                "created_at" timestamptz NULL,
-                "updated_at" timestamptz NULL,
+                "created_at" timestamptz NULL DEFAULT now(),
+                "updated_at" timestamptz NULL DEFAULT now(),
                 "isAdmin" boolean NOT NULL DEFAULT false,
                 "guild_id" integer NOT NULL,
                 "user_id" integer NOT NULL
